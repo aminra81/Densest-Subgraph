ef18eefd8e07ca18abf4c8bd73599a27bacfe53a
Mehrshad-Taziki <75726578+Mehrshad-Taziki@users.noreply.github.com>
2024-07-31 17:33:26 +0330

    Implemented the Hybrid Algorithm

--- algorithms/hybrid.py
+++ algorithms/hybrid.py
@@ -1,8 +1,23 @@
+from algorithms.goldberg import goldberg_algorithm
+import math
+
 import llist
 
 
-def is_sparse(edges, set_len):
-    pass
+def constructGraph(number_of_vertices, adjacency_graph, is_deleted, current_number_of_vertices):
+    current_adjacency_graph = [[] for _ in range(current_number_of_vertices + 1)]
+    index = dict()
+    count = 0
+    for current_vertex in range(1, number_of_vertices + 1):
+        if not is_deleted[current_vertex]:
+            count += 1
+            index[current_vertex] = count
+    for current_vertex in range(number_of_vertices + 1):
+        if not is_deleted[current_vertex]:
+            for neighbor in adjacency_graph[current_vertex]:
+                if not is_deleted[neighbor]:
+                    current_adjacency_graph[index[current_vertex]].append(index[neighbor])
+    return current_adjacency_graph
 
 
 def hybrid(number_of_vertices, adjacency_graph):
@@ -18,7 +33,10 @@ def hybrid(number_of_vertices, adjacency_graph):
         nodes.append(new_node)
     min_deg = 0
     i = number_of_vertices
+    limit = math.sqrt(number_of_vertices)
+    checked = False
     been = dict()
+    max_ans = 0.0
     while i > 1:
         for deg in range(min_deg, number_of_vertices):
             if len(list_of_degrees[deg]) > 0:
@@ -37,7 +55,11 @@ def hybrid(number_of_vertices, adjacency_graph):
                 min_deg = deg - 1
                 break
         i = i - 1
-    max_ans = 0.0
+        if i <= limit and not checked:
+            checked = True
+            limit_adjacency_graph = constructGraph(number_of_vertices, adjacency_graph, is_deleted, i)
+            answer = goldberg_algorithm(i, limit_adjacency_graph)
+
     is_in_set = [0 for _ in range(number_of_vertices + 1)]
     for i in range(number_of_vertices + 1):
         if is_deleted[i] == 0:
@@ -51,10 +73,6 @@ def hybrid(number_of_vertices, adjacency_graph):
             if is_in_set[neighbor]:
                 edges = edges + 1
         set_len = len(answer) - i + 1
-        if is_sparse(edges, set_len):
-            new_adjacency = []
-            for v in range(number_of_vertices + 1):
-                new_adjacency.append([])
         '''if edges / set_len > 35.0:
             is_there = []
             for t in range(1, number_of_vertices + 1):
