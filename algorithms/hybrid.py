5e1d0b1ce03445cc2092c5ddd8ac31899ad7e340
Mehrshad-Taziki <75726578+Mehrshad-Taziki@users.noreply.github.com>
2024-08-19 18:22:55 +0330

    Implemented the Iterative-Hybrid Algorithm

--- algorithms/hybrid.py
+++ algorithms/hybrid.py
@@ -1,6 +1,5 @@
 from algorithms.goldberg import goldberg_algorithm
 import math
-
 import llist
 
 
@@ -37,6 +36,7 @@ def hybrid(number_of_vertices, adjacency_graph):
     checked = False
     been = dict()
     max_ans = 0.0
+    goldberg_answer = 0.0
     while i > 1:
         for deg in range(min_deg, number_of_vertices):
             if len(list_of_degrees[deg]) > 0:
@@ -58,7 +58,7 @@ def hybrid(number_of_vertices, adjacency_graph):
         if i <= limit and not checked:
             checked = True
             limit_adjacency_graph = constructGraph(number_of_vertices, adjacency_graph, is_deleted, i)
-            answer = goldberg_algorithm(i, limit_adjacency_graph)
+            goldberg_answer = goldberg_algorithm(i, limit_adjacency_graph)
 
     is_in_set = [0 for _ in range(number_of_vertices + 1)]
     for i in range(number_of_vertices + 1):
@@ -73,10 +73,55 @@ def hybrid(number_of_vertices, adjacency_graph):
             if is_in_set[neighbor]:
                 edges = edges + 1
         set_len = len(answer) - i + 1
-        '''if edges / set_len > 35.0:
-            is_there = []
-            for t in range(1, number_of_vertices + 1):
-                if not is_deleted[t]:
-                    is_there.append(t)'''
         max_ans = max(max_ans, edges / set_len)
+    return max(goldberg_answer, max_ans)
+
+
+def iterative_hybrid(number_of_vertices, adjacency_graph, T):
+    load = [0 for i in range(number_of_vertices + 1)]
+    deg = [0 for i in range(number_of_vertices + 1)]
+    deleted_vertex = [[0 for j in range(number_of_vertices + 1)] for i in range(T + 1)]
+    goldberg_answers = [0 for i in range(T + 1)]
+    limit = math.sqrt(number_of_vertices)
+
+    for i in range(1, T + 1):
+        current_number_of_vertices = number_of_vertices
+        checked = False
+        is_deleted = [0 for i in range(number_of_vertices + 1)]
+        pq = []
+        for j in range(1, number_of_vertices + 1):
+            deg[j] = len(adjacency_graph[j])
+            pq.append((load[j] + deg[j], j))
+        heapq.heapify(pq)
+        j = 0
+        while pq:
+            vertex = heapq.heappop(pq)
+            if is_deleted[vertex]:
+                continue
+            j = j + 1
+            is_deleted[vertex] = 1
+            current_number_of_vertices -= 1
+            deleted_vertex[i][j] = vertex
+            load[vertex] = load[vertex] + deg[vertex]
+            for adj_vertex in adjacency_graph[vertex]:
+                if not is_deleted[adj_vertex]:
+                    deg[adj_vertex] = deg[adj_vertex] - 1
+                    heapq.heappush(pq, (load[adj_vertex] + deg[adj_vertex], adj_vertex))
+            if current_number_of_vertices <= limit and not checked:
+                checked = True
+                limit_adjacency_graph = constructGraph(number_of_vertices, adjacency_graph, is_deleted, i)
+                goldberg_answers[i] = goldberg_algorithm(current_number_of_vertices, limit_adjacency_graph)
+    max_ans = 0
+    for i in range(1, T + 1):
+        is_in_set = [0 for _ in range(number_of_vertices + 1)]
+        edges = 0
+        for j in range(number_of_vertices, 0, -1):
+            current_vertex = deleted_vertex[i][j]
+            is_in_set[current_vertex] = 1
+            for neighbor in adjacency_graph[current_vertex]:
+                if is_in_set[neighbor]:
+                    edges = edges + 1
+            set_len = number_of_vertices - j + 1
+            max_ans = max(max_ans, edges / set_len)
+            max_ans = max(max_ans, goldberg_answers[i])
     return max_ans
